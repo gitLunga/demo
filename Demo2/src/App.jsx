357f68d596adf8bc606b085044223fba412b63c2
Lunga Ntshingila <lungamngomezulu10@gmail.com>
2026-03-03 18:55:42 +0200

    browse router 2

--- Demo2/src/App.jsx
+++ Demo2/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Routes, Route, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route,  } from 'react-router-dom';
 import Home from './pages/Home';
 // import './App.css'
 
